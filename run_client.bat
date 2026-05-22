db67d9c954ead4c1ef0c1ce45bc7cdb7fa959d93
ngocnguyen2011 <khanhnguyentho2po@gmail.com>
2026-05-22 21:26:06 +0200

    Add comments for 2 supporting bat files

--- run_client.bat
+++ run_client.bat
@@ -1,4 +1,7 @@
-@echo off
+// This batch file is used to run the client application for the cardio generator project. It executes the main class with the argument "DataStorage" to start the client that will receive and store data from the server.
+// Just a supporting file for running
+
+@echo off 
 echo ====================================================
 echo BAT CLIENT DATA TAKE IN...
 echo ====================================================
--- run_server.bat
+++ run_server.bat
@@ -1,3 +1,6 @@
+// This batch file is used to run the server application for the cardio generator project. It executes the main class with the argument "websocket:8080" to start the server that will send data to clients via WebSocket on port 8080.
+// Just a supporting file for running
+
 @echo off
 echo ====================================================
 echo BAT SERVER SIGNALING...
